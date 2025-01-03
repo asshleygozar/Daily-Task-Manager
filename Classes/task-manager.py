0fd5d7a243436df674886036ea61af81d367ebf0
Asshley <asshleydeanwaynegozar@gmail.com>
2025-01-03 08:27:38 +0800

    Initialized main and sub classes

---
+++ Classes/task-manager.py
@@ -0,0 +1,24 @@
+
+
+class TaskManager:
+
+    def __init__(self):
+        pass
+
+    def load_tasks():
+        pass
+
+    def add_tasks():
+        pass
+
+    def view_tasks():
+        pass
+
+    def update_tasks_status():
+        pass
+
+    def filter_by_due_date():
+        pass
+
+    def filter_by_priority():
+        pass
