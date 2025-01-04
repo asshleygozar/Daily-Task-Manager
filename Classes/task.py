328860d97fb9bd20ee18f93206a7279b0b2c16b7
Asshley <asshleydeanwaynegozar@gmail.com>
2025-01-04 21:29:38 +0800

    Initialized and programmed action function

--- Classes/task.py
+++ Classes/task.py
@@ -1,8 +1,38 @@
 
+class Task():
 
-class Task:
+    def action():
 
-    def __init__(self):
-        pass
+        while True:
+
+            try:
+
+                print("Choose your action")
+                print("1. Add Task")
+                print("2. View Task")
+                print("3. Update Task")
+                print("4. Delete Task")
+                print("5. Exit")
+
+                user_input = int(input("Enter your choice: "))
+
+                match user_input:
+
+                    case 1:
+                        pass
+                    case 2:
+                        pass
+                    case 3:
+                        pass
+                    case 4:
+                        pass
+                    case 5:
+                        pass
+                    case _:
+                        print("Invalid Input!")
+
+            except ValueError:
+
+                print("Integer number only!")
 
     
