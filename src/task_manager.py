2ecdcd869c7533e7d3543905ea83787c2c8066a1
Asshley <asshleydeanwaynegozar@gmail.com>
2025-01-13 12:33:41 +0800

    Optimized add task function

--- src/task_manager.py
+++ src/task_manager.py
@@ -1,35 +1,46 @@
 import csv
+import os
 import time
 
 class TaskManager():
 
-    def __init__(self, task, description="None", priority="Low", deadline="Now"):
+    def __init__(self, task="Not Provided", description="None", priority="Low", deadline="Now"):
         self.task = task
         self.description = description
         self.priority = priority
         self.deadline = deadline
-        self.number = 0
-        self.file_path = "G:\\Daily-Task-Manager\\Database\\task-records.csv"
         
     def __str__(self):
         return f"{self.task} | {self.description} | {self.priority} | {self.deadline}"
     
+    def data_file_path():
+        __file_path = "G:\\Daily-Task-Manager\\Database\\task-records.csv"
+        return __file_path
+    
     def add_task(self):
+
         self.task = input("Enter task name: ")
-        self.description = input("Enter task description")
+        self.description = input("Enter task description: ")
         self.priority = input("Enter task priority level: ")
         self.deadline = input("Enter task deadline (MM/DD/YYYY): ")
         
-        task_number = self.number =+ 1
-        task_data = [{task_number:
-                       {"Task":self.task,
-                        "Description":self.description,
-                        "Priority":self.priority,
-                        "Deadline":self.deadline
-                        }}]
+        task_data = {   
+                     "Task":self.task,
+                     "Description":self.description,
+                     "Priority":self.priority,
+                     "Deadline":self.deadline
+                        }
 
-        with open(self.file_path, mode='w', newline='') as file:
-            writer = csv.write(file)
+        with open(TaskManager.data_file_path(), mode='a', newline='') as file:
+            field_names = ["Task","Description","Priority","Deadline"] 
+            writer = csv.DictWriter(file,fieldnames=field_names)
+
+            writer.writeheader()
+            writer.writerow(task_data)
+
+
+
+            
 
 
 
